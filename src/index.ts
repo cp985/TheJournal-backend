1326530387b9af68319f0f6ace6b6a909885157d
cp985 <166384434+cp985@users.noreply.github.com>
2026-07-29 17:03:21 +0200

    init corse with vercel link

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
 import express from "express";
 import router from "./routers/mainRouters.js"
+import cors from "cors"
+import dotenv from 'dotenv';
+dotenv.config();
 
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors( {
+    origin: process.env.NODE_ENV === "development" ? "http://localhost:3000" : process.env.VERCEL_URL,
+    credentials: true
+}));
 
 
 
