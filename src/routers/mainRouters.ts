d583300920b4c6d7a70965364645de6c57d64a82
cp985 <166384434+cp985@users.noreply.github.com>
2026-08-03 15:56:29 +0200

    change router path

--- src/routers/mainRouters.ts
+++ src/routers/mainRouters.ts
@@ -2,10 +2,12 @@ import express from "express";
 import * as mainControllers from "../controllers/mainControllers.js";
 
 const router = express.Router();
+const path = process.env.NODE_ENV === "development" ? "" : process.env.URL_RENDER
 
-router.get("/",mainControllers.homeGet); 
-router.get("/users",mainControllers.usersGet); 
-router.post("/users",mainControllers.usersPost); 
+
+router.get(path + "/",mainControllers.homeGet); 
+router.get(path + "/users",mainControllers.usersGet); 
+router.post( path + "/users",mainControllers.usersPost); 
 
 
 export default router;
