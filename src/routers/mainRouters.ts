976e92ca295a3be813b04a8a0b3c756e1a2b5801
cp985 <166384434+cp985@users.noreply.github.com>
2026-08-11 14:56:49 +0200

    improve oauth provider in db and password?, add user/oauth-sync

--- src/routers/mainRouters.ts
+++ src/routers/mainRouters.ts
@@ -9,9 +9,11 @@ router.post("/user/email",mainControllers.usersByEmail);
 router.get("/users",mainControllers.usersGet); 
 router.post("/users/signup",mainControllers.usersSignUp); 
 router.post("/users/login",mainControllers.usersLogin);
+router.post('/user/oauth-sync',mainControllers.usersOAuthSync);
 router.post("/dossiers",mainControllers.dossiersPost);
 router.get("/dossiers",mainControllers.dossiersGet);
 router.post("/evidences",mainControllers.evidencesPost);
 router.post("/contact",mainControllers.emailResend);
+
 
 export default router;
